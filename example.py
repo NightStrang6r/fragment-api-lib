1e2b38030f0cf9e5684fe79e32c8a1ef6e785480
NightStrang6r <47156355+NightStrang6r@users.noreply.github.com>
2025-05-14 17:26:26 +0100

    ♨️ Update example

--- example.py
+++ example.py
@@ -6,49 +6,56 @@ client = FragmentAPIClient()
 # Ping
 print("API ping:", client.ping())
 
+# Replace with your 24 words seed phrase from TON v4r2 Wallet
+seed = "your_24_words_seed_phrase"
+
+# Replace with your Fragment cookies exported from Cookie-Editor extension as Header String
+# https://chromewebstore.google.com/detail/cookie-editor/hlkenndednhfkekhgcdicdfddnkalmdm
+fragment_cookies = "your_fragment_cookies"
+
 # Get balance
-res = client.get_balance(seed="your_24_words_seed_phrase")
+res = client.get_balance(seed=seed)
 print("Balance:", res)
 
 # Get user info
 res = client.get_user_info(
-    username="durov", # or "@durov", or "https://t.me/durov"
-    fragment_cookies="your_fragment_cookies"
+    username="NightStrang6r", # or "@NightStrang6r", or "https://t.me/NightStrang6r"
+    fragment_cookies=fragment_cookies
 )
 print("User info:", res)
 
 # Buy stars without KYC
 res = client.buy_stars_without_kyc(
-    username="durov", # or "@durov", or "https://t.me/durov"
+    username="NightStrang6r", # or "@NightStrang6r", or "https://t.me/NightStrang6r"
     amount=100,
-    seed="your_24_words_seed_phrase"
+    seed=seed
 )
 print("Buy stars without KYC response:", res)
 
 # Buy stars
 res = client.buy_stars(
-    username="durov", # or "@durov", or "https://t.me/durov"
+    username="NightStrang6r", # or "@NightStrang6r", or "https://t.me/NightStrang6r"
     amount=100,
     show_sender=False,
-    fragment_cookies="your_fragment_cookies",
-    seed="your_24_words_seed_phrase"
+    fragment_cookies=fragment_cookies,
+    seed=seed
 )
 print("Buy stars response:", res)
 
 # Buy Telegram Premium without KYC
 res = client.buy_premium_without_kyc(
-    username="durov", # or "@durov", or "https://t.me/durov"
+    username="NightStrang6r", # or "@NightStrang6r", or "https://t.me/NightStrang6r"
     duration=3, # 3 or 6 or 12 months
-    fragment_cookies="your_fragment_cookies",
-    seed="your_24_words_seed_phrase"
+    fragment_cookies=fragment_cookies,
+    seed=seed
 )
 print("Buy Telegram Premium without KYC response:", res)
 
 # Buy Telegram Premium
 res = client.buy_premium(
-    username="durov", # or "@durov", or "https://t.me/durov"
+    username="NightStrang6r", # or "@NightStrang6r", or "https://t.me/NightStrang6r"
     duration=3, # 3 or 6 or 12 months
     show_sender=False,
-    fragment_cookies="your_fragment_cookies",
-    seed="your_24_words_seed_phrase"
+    fragment_cookies=fragment_cookies,
+    seed=seed
 )
